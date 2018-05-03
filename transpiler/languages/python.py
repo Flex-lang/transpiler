8f017d0e6b62c42439acc5e54b59f4516694b1ec
Faheel Ahmad <faheel@live.in>
2018-05-03 14:54:14 +0530

    Add Python code more intents
    * print_elements
    * input
    * begin_for_each
    * begin_while
    * initialize_assign

--- transpiler/languages/python.py
+++ transpiler/languages/python.py
@@ -1,6 +1,40 @@
 code = {
+    # Output
     'print': {
         'entities': ['to_print'],
         'code': 'print({to_print})'
-    }
+    },
+    'print_elements': {
+        'entities': ['to_print'],
+        'code': '''
+            for element in {to_print}:
+                print(element, end=', ')
+            print('\b\b ')
+        '''
+    },
+
+    # Input
+    'input': {
+        'entities': ['var_name'],
+        'code': 'input({var_name})'
+    },
+    # TODO: multiple input
+
+    # Loops
+    # for each
+    'begin_for_each': {
+        'entities': ['loop_over', 'loop_as'],
+        'code': 'for {loop_as} in {loop_over}:'
+    },
+    # while
+    'begin_while': {
+        'entities': ['condition'],
+        'code': 'while {condition}:'
+    },
+
+    # Assignment / initialisation
+    'initialize_assign': {
+        'entities': ['var_name', 'var_value'],
+        'code': '{var_name} = {var_value}'
+    },
 }
