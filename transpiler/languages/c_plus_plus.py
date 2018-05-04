c3f9415ce705441b883564805c05c8ea45bd0c89
thegenuinegourav <gourav96suri@gmail.com>
2018-05-04 14:27:21 +0530

    Added c++ dictionary

--- transpiler/languages/c_plus_plus.py
+++ transpiler/languages/c_plus_plus.py
@@ -2,39 +2,41 @@ code = {
     # Output
     'print': {
         'entities': ['to_print'],
-        'code': 'print({to_print})'
+        'code': 'cout << {to_print};'
     },
+
+    # TODO: data type to use in looping, by default it's int.
     'print_elements': {
         'entities': ['to_print'],
         'code': '''
-            for element in {to_print}:
-                print(element, end=', ')
-            print('\b\b ')
+            for(const int &element : {to_print})
+                cout << element << endl;            
         '''
     },
 
     # Input
     'input': {
         'entities': ['var_name'],
-        'code': 'input({var_name})'
+        'code': 'cin >> {var_name};'
     },
     # TODO: multiple input
 
     # Loops
     # for each
+    # TODO: data type to use in looping, by default it's int.
     'begin_for_each': {
         'entities': ['loop_over', 'loop_as'],
-        'code': 'for {loop_as} in {loop_over}:'
+        'code': 'for(int {loop_as} : {loop_over})'
     },
     # while
     'begin_while': {
         'entities': ['condition'],
-        'code': 'while {condition}:'
+        'code': 'while({condition})'
     },
 
     # Assignment / initialisation
     'initialize_assign': {
         'entities': ['var_name', 'var_value'],
-        'code': '{var_name} = {var_value}'
+        'code': '{var_name} = {var_value};'
     },
 }
