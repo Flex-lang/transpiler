021ce6f423a0a372761d5a3440327bf96140592f
thegenuinegourav <gourav96suri@gmail.com>
2018-05-04 18:49:54 +0530

    update changes as per merge requests

--- transpiler/languages/c_plus_plus.py
+++ transpiler/languages/c_plus_plus.py
@@ -2,36 +2,36 @@ code = {
     # Output
     'print': {
         'entities': ['to_print'],
-        'code': 'cout << {to_print};'
+        'code': 'std::cout << {to_print};'
     },
 
-    # TODO: data type to use in looping, by default it's int.
+    
     'print_elements': {
         'entities': ['to_print'],
         'code': '''
-            for(const int &element : {to_print})
-                cout << element << endl;            
+            for (const auto& element : {to_print})
+    			std::cout << element << ", ";
+			std::cout << "\b\b \n";           
         '''
     },
 
     # Input
     'input': {
         'entities': ['var_name'],
-        'code': 'cin >> {var_name};'
+        'code': 'std::cin >> {var_name};'
     },
     # TODO: multiple input
 
     # Loops
     # for each
-    # TODO: data type to use in looping, by default it's int.
     'begin_for_each': {
         'entities': ['loop_over', 'loop_as'],
-        'code': 'for(int {loop_as} : {loop_over})'
+        'code': 'for(const auto& {loop_as} : {loop_over}) {'
     },
     # while
     'begin_while': {
         'entities': ['condition'],
-        'code': 'while({condition})'
+        'code': 'while({condition}) {'
     },
 
     # Assignment / initialisation
